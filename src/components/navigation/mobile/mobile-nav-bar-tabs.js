61688b8078c3336f7c48eff3f3ace43ecbec7323
malexj <alexmaximovj@gmail.com>
2024-09-10 02:01:00 +0300

    Fix mobile preview

--- src/components/navigation/mobile/mobile-nav-bar-tabs.js
+++ src/components/navigation/mobile/mobile-nav-bar-tabs.js
@@ -1,36 +1,38 @@
-import { useAuth0 } from "@auth0/auth0-react";
+import {useAuth0} from "@auth0/auth0-react";
 import React from "react";
-import { MobileNavBarTab } from "./mobile-nav-bar-tab";
+import {MobileNavBarTab} from "./mobile-nav-bar-tab";
+import {NavBarTab} from "../desktop/nav-bar-tab";
 
-export const MobileNavBarTabs = ({ handleClick }) => {
-  const { isAuthenticated } = useAuth0();
+export const MobileNavBarTabs = ({handleClick}) => {
+    const {isAuthenticated} = useAuth0();
 
-  return (
-    <div className="mobile-nav-bar__tabs">
-      <MobileNavBarTab
-        path="/profile"
-        label="Profile"
-        handleClick={handleClick}
-      />
-      <MobileNavBarTab
-        path="/public"
-        label="Public"
-        handleClick={handleClick}
-      />
-      {isAuthenticated && (
-        <>
-          <MobileNavBarTab
-            path="/protected"
-            label="Protected"
+    return (<div className="mobile-nav-bar__tabs">
+        <MobileNavBarTab
+            path="/profile"
+            label="Profile"
             handleClick={handleClick}
-          />
-          <MobileNavBarTab
-            path="/admin"
-            label="Admin"
+        />
+        <NavBarTab
+            path="/calendar"
+            label="Calendar"
             handleClick={handleClick}
-          />
-        </>
-      )}
-    </div>
-  );
+        />
+        <MobileNavBarTab
+            path="/public"
+            label="Public"
+            handleClick={handleClick}
+        />
+        {isAuthenticated && (<>
+            <MobileNavBarTab
+                path="/protected"
+                label="Protected"
+                handleClick={handleClick}
+            />
+            <MobileNavBarTab
+                path="/admin"
+                label="Admin"
+                handleClick={handleClick}
+            />
+        </>)}
+    </div>);
 };
